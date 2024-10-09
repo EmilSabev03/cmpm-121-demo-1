2e444f9ac5f2c4d11d44737363f5497f06683c10
Emil Sabev <emozconfidence@gmail.com>
2024-10-09 11:24:32 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,31 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//add counter
+let counter: number = 0;
+
+//add div 
+const div = document.createElement("div");
+div.innerHTML = `${counter} pizza slices`;
+app.appendChild(div);
+
 //add button to webpage
 const button = document.createElement("button");
-
 button.innerHTML = "🍕";
-app.appendChild(button);
+app.appendChild(button); 
 
-button.addEventListener("click", () => {
-  console.log("Button clicked");
+//check if button is clicked
+button.addEventListener("click", () => 
+{
+    //increase counter and use proper wording
+    counter += 1;
+    if (counter == 1)
+    {
+        div.innerHTML = `${counter} pizza slice`;
+    }
+
+    else
+    {
+        div.innerHTML = `${counter} pizza slices`;
+    }
 });
