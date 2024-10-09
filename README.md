97834d1bbf79ca7f690845beea67b11f981d8569
Emil Sabev <emozconfidence@gmail.com>
2024-10-09 13:46:42 -0700

    Step 5 complete

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This is the starter code project for the `Demo 1` project in CMPM 121, Game Development Patterns, at UC Santa Cruz (fall quarter, 2024). Students should incrementally modify this project, tracking their progress with git, to develop an incremental game of their own unique design.
+Name: Emil Sabev  
+Assignment: D1.a: Incremental Game Development, Steps 1 to 5  
+Description: Short but functional pizza slice clicker game with upgrade  
+  
+Credits:  
+  
+-ChatGPT and MDN helped with step 4:  
+https://chatgpt.com/share/6706d93f-51bc-8010-9158-6c8203fea11a
+https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame  
+  
+-ChatGPT and MDN helped with step 5:  
+https://chatgpt.com/share/6706ea0a-93cc-8010-909e-1875255f3aca
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled  
+  
--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,77 @@
-/*
-credits:
-
--ChatGPT and MDN helped with step 4: 
-https://chatgpt.com/share/6706d93f-51bc-8010-9158-6c8203fea11a
-https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
-
-
-
-*/
-
-
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Pizza clicker";
+const gameName = "Pizza Clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//add counter, lastTime, interval
+
+//add counter, lastTime, growthRate
 let counter: number = 0;
 let lastTime = performance.now();
+let growthRate: number = 0;
 
 //add div
 const div = document.createElement("div");
 div.innerHTML = `${counter} pizza slices`;
 app.appendChild(div);
 
-//add button to webpage
-const button = document.createElement("button");
-button.innerHTML = "🍕";
-app.appendChild(button);
 
-//function to format display of counter
-function formatDisplay(counter: number): string {
-  if (counter === 1) {
-    return `${counter} pizza slice`;
-  } else {
-    return `${counter} pizza slices`;
-  }
-}
+//add pizza clicker button to webpage
+const click = document.createElement("button");
+click.innerHTML = "🍕";
+app.appendChild(click);
 
-//Increment counter once per second using frame count
+
+//add upgrade button to webpage
+const upgrade = document.createElement("button");
+upgrade.disabled = true;
+upgrade.innerHTML = "Click here to upgrade";
+app.appendChild(upgrade);
+
+
+//Function to increment counter if counter > 10
 function update() 
 {
+    upgrade.disabled = counter < 10;
+
     const time = performance.now();
     const deltaTime = time - lastTime;
-    
-    counter += deltaTime / 1000;
+
+    counter += growthRate * (deltaTime / 1000);
     div.innerHTML = formatDisplay(counter);
     lastTime = time;
-  
+
     requestAnimationFrame(update);
-}
-  
+}   
+
 requestAnimationFrame(update);
-  
-//check for button clicks
-button.addEventListener("click", () => 
-{
+
+
+//Function to format the display
+const formatDisplay = (number: number): string => {
+    if (number === 1)
+    {
+        return Math.floor(number).toString() + " slice"; 
+    }
+
+    return Math.floor(number).toString() + " slices"; 
+};
+
+//Check for pizza button clicks
+click.addEventListener("click", () => {
     counter += 1;
     div.innerHTML = formatDisplay(counter);
 });
+
+//Check for upgrade button clicks
+upgrade.addEventListener("click", () => {
+    counter -= 10;
+    growthRate += 1;
+    div.innerHTML = formatDisplay(counter);
+});
+
