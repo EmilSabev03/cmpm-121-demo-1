b8307416e66f9c0526160e19f34526a1adcc0f6e
Emil Sabev <emozconfidence@gmail.com>
2024-10-09 11:09:45 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,13 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//add button to webpage
+const button = document.createElement('button');
+
+button.innerHTML = '🍕';
+app.appendChild(button);
+
+button.addEventListener('click', () => {
+  console.log('Button clicked');
+});
