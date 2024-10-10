571f6d0abf958a4d32f642013b358475e7b5b8bb
Emil Sabev <emozconfidence@gmail.com>
2024-10-10 11:53:32 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -9,38 +9,44 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//add counter, lastTime, growthRate, upgrade values
+
+//add counter, lastTime, growthRate, upgrade, priceGrowth values
 let counter: number = 0;
 let lastTime = performance.now();
 let growthRate: number = 0;
-let A: number = 0;
-let B: number = 0;
-let C: number = 0;
+let A = 0, B = 0, C = 0;
+let priceGrowthA = 1, priceGrowthB = 1, priceGrowthC = 1;
+
 
 //display amount of slices
 const div = document.createElement("div");
 div.innerHTML = `${counter} pizza slices`;
 app.appendChild(div);
 
+
 //display growth rate
 const growthRateDiv = document.createElement("div");
 growthRateDiv.innerHTML = `${growthRate.toFixed(1)} slices/sec`;
 app.appendChild(growthRateDiv);
+
 
 //add pizza clicker button to webpage
 const click = document.createElement("button");
 click.innerHTML = "🍕";
 app.appendChild(click);
 
+
 //add upgrade buttons to webpage
 const upgradeA = createUpgradeButton("A", true, app);
 const upgradeB = createUpgradeButton("B", true, app);
 const upgradeC = createUpgradeButton("C", true, app);
 
+
 //display upgrade count
 const amountUpgradeA = displayUpgradeCount("A", A, app);
 const amountUpgradeB = displayUpgradeCount("B", B, app);
 const amountUpgradeC = displayUpgradeCount("C", C, app);
+
 
 //Function to simplify creating upgrade buttons
 function createUpgradeButton(
@@ -55,7 +61,8 @@ function createUpgradeButton(
   return upgradeX;
 }
 
-//Function to display upgrade count
+
+//Function to simplify display upgrade count
 function displayUpgradeCount(
   upgradeName: string,
   count: number,
@@ -67,11 +74,12 @@ function displayUpgradeCount(
   return amountUpgradeX;
 }
 
+
 //Function to handle upgrades and updates
 function update() {
-  upgradeA.disabled = counter < 10;
-  upgradeB.disabled = counter < 100;
-  upgradeC.disabled = counter < 1000;
+  upgradeA.disabled = counter < 10 * priceGrowthA;
+  upgradeB.disabled = counter < 100 * priceGrowthB;
+  upgradeC.disabled = counter < 1000 * priceGrowthC;
 
   const time = performance.now();
   const deltaTime = time - lastTime;
@@ -85,6 +93,7 @@ function update() {
 
 requestAnimationFrame(update);
 
+
 //Function to format the display
 const formatDisplay = (number: number): string => {
   if (number === 1) {
@@ -94,17 +103,20 @@ const formatDisplay = (number: number): string => {
   return Math.floor(number).toString() + " slices";
 };
 
+
 //Check for pizza button clicks
 click.addEventListener("click", () => {
   counter += 1;
   div.innerHTML = formatDisplay(counter);
 });
 
+
 //Check for upgrade button clicks
 upgradeA.addEventListener("click", () => {
   A += 1;
   counter -= 10;
   growthRate += 0.1;
+  priceGrowthA *= 1.15;
   div.innerHTML = formatDisplay(counter);
   growthRateDiv.innerHTML = `${growthRate.toFixed(1)} slices/sec`;
   amountUpgradeA.innerHTML = `A: ${A}`;
@@ -114,6 +126,7 @@ upgradeB.addEventListener("click", () => {
   B += 1;
   counter -= 100;
   growthRate += 2;
+  priceGrowthB *= 1.15;
   div.innerHTML = formatDisplay(counter);
   growthRateDiv.innerHTML = `${growthRate.toFixed(1)} slices/sec`;
   amountUpgradeB.innerHTML = `B: ${B}`;
@@ -123,6 +136,7 @@ upgradeC.addEventListener("click", () => {
   C += 1;
   counter -= 1000;
   growthRate += 50;
+  priceGrowthC *= 1.15;
   div.innerHTML = formatDisplay(counter);
   growthRateDiv.innerHTML = `${growthRate.toFixed(1)} slices/sec`;
   amountUpgradeC.innerHTML = `C: ${C}`;
